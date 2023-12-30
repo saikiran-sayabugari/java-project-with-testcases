6272d67ce7235c7be18fac5b39dc1e7ad8b78a42
saikiran-sayabugari <94823927+saikiran-sayabugari@users.noreply.github.com>
2023-12-31 00:35:02 +0530

    Update WebServer.java

--- src/main/java/com/kiran/WebServer.java
+++ src/main/java/com/kiran/WebServer.java
@@ -3,20 +3,21 @@ package com.kiran;
 import com.google.gson.Gson;
 import com.kiran.controller.ItemController;
 import com.kiran.repository.ItemRepository;
-import spark.Route;
 import spark.Spark;
 
 public class WebServer {
-
     public static void main(String[] args) {
-        // Set up Spark server
+        // Create instances of dependencies
+        ItemRepository itemRepository = new ItemRepository();
+        Gson gson = new Gson();
+
+        // Create ItemController with dependencies
+        ItemController itemController = new ItemController(itemRepository, gson);
+
+        // Set up Spark routes
         Spark.port(8080);
 
-        // Initialize data repository and controller
-        ItemRepository itemRepository = new ItemRepository();
-        ItemController itemController = new ItemController(itemRepository);
-
-        // Set up RESTful endpoints
+        // Define your routes using ItemController
         Spark.get("/items", itemController.getAllItems);
         Spark.get("/items/:id", itemController.getItemById);
         Spark.post("/items", itemController.addItem);
@@ -24,4 +25,3 @@ public class WebServer {
         Spark.delete("/items/:id", itemController.deleteItem);
     }
 }
-
